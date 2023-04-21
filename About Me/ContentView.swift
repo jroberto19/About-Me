0a48735529963a327368ea7fabe21a02a79bf48e
Julia Roberto <juliaannroberto@gmail.com>
2023-04-21 13:32:36 -0400

    Initial commit

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -1,21 +1,115 @@
-//
-//  ContentView.swift
-//  About Me
-//
-//  Created by Julia Roberto on 4/20/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            VStack {
+                Text("Julia Roberto")
+                    .font(.largeTitle)
+                    .foregroundColor(.pink)
+                
+                Image("aboutMe")
+                    .resizable()
+                    .frame(width: 350, height: 400)
+                    .border(Color.pink, width: 10)
+                
+                VStack {
+                    Text("Do you want to learn more about me?")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.blue, lineWidth: 2)
+                        )
+                    
+                    Button("Yes, please!") {
+                        self.showDetails = true
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding()
+                }
+            }
         }
-        .padding()
+        .sheet(isPresented: $showDetails) {
+            VStack {
+                Text("About Me")
+                    .font(.title)
+                    .padding()
+                
+                VStack {
+                    HStack {
+                        Image("sokkaAndMikasa")
+                            .resizable(resizingMode: .stretch)
+                            
+                            .padding()
+                        
+                        Text("I love my cats, Sokka and Mikasa")
+                            .font(.headline)
+                            .padding()
+                    }
+                    .padding()
+                    .background(Color.yellow)
+                    .cornerRadius(10)
+                    
+                    HStack {
+                        Image("fangirlFantasy")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                        
+                        Text("I love concerts, music festivals, and fangirl nights")
+                            .font(.headline)
+                            .padding()
+                    }
+                    .padding()
+                    .background(Color.yellow)
+                    .cornerRadius(10)
+                    
+                    HStack {
+                        Image("nonna")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                        
+                        Text("My Nonna is my best friend")
+                            .font(.headline)
+                            
+                    }
+                    .padding()
+                    .background(Color.yellow)
+                    .cornerRadius(10)
+                    
+                    HStack {
+                        Image("nature")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                        
+                        Text("I love being in nature")
+                            .font(.headline)
+                            .padding()
+                    }
+                    .padding()
+                    .background(Color.yellow)
+                    .cornerRadius(10)
+                }
+                
+                Button("Go Back") {
+                    self.showDetails = false
+                }
+                .padding()
+                .background(Color.yellow)
+                .cornerRadius(10)
+                .padding()
+            }
+        }
     }
 }
 
