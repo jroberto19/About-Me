76d484c91275c9ead454fb1875851fc4d92d4b27
Julia Roberto <juliaannroberto@gmail.com>
2023-04-20 20:58:42 -0400

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Julia Roberto on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
